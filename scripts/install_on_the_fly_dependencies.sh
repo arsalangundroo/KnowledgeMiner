b270b9038d575a035fca0cf553842d55ab90e606
gar1syv <arsalan.gundroo@us.bosch.com>
2024-09-07 15:18:08 -0700

    adds script for laoding dependenceis

---
+++ scripts/install_on_the_fly_dependencies.sh
@@ -0,0 +1,7 @@
+conda create -n "know_miner" python==3.11
+
+conda activate know_miner
+pip3 install llama-index-vector-stores-chroma
+pip3 install llama-index-embeddings-azure-openai
+pip3 install llama-index-llms-azure-openai
+
